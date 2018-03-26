398b5f7aa6e6d7e1fb5d9105b240ca23e92edb22
Russell Parker <russell.parker7@gmail.com>
2018-03-25 22:36:28 -0400

    Lots of Spotify connection setup

--- server.py
+++ server.py
@@ -1,15 +1,45 @@
 #!/usr/bin/env python
 
+import sys
 import spotipy
 import spotipy.util
 
-scope = 'user-library-read'
-redirect = 'http://localhost/'
-username = raw_input('What is your Spotify username?\n> ') 
+# --------------------
+# perform Spotify setup
+# --------------------
 
+PLAYLIST_NAME = 'networks-playlist'
+
+# prompt for username and attempt authentication
+username = raw_input('What is your Spotify username?\n> ') 
+scope = 'user-modify-playback-state playlist-modify-public user-modify-playback-state user-modify-playback-state'
+redirect = 'http://localhost/'
 token = spotipy.util.prompt_for_user_token(username, scope, redirect_uri=redirect)
 
-if token:
-    print "successfully retrieved token"
+# if authentication failed
+if not token:
+    sys.stderr.write('Could not authenticate')
+    sys.exit()
+
+# initiate Spotify client
+sp = spotipy.Spotify(auth=token)
+user = sp.me()['id']
+
+# get all playlists that match the name PLAYLIST_NAME
+playlists = filter(lambda pl: pl['name'] == PLAYLIST_NAME, sp.current_user_playlists()['items'])
+
+# grab first result if there is one, otherwise create it
+if playlists:
+    pl = playlists[0]
 else:
-    print "failed to retrieve token"
+    pl = sp.user_playlist_create(user, PLAYLIST_NAME)
+
+# start or resume the playlist
+sp.start_playback(context_uri=pl['uri'])
+
+
+# --------------------
+# enter loop
+# --------------------
+
+# sp.user_playlist_add_tracks(user, pl['id'])
