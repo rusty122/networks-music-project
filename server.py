6058aee5728ac54ba05fdfe2d7b286315762ec6f
Russell Parker <russell.parker7@gmail.com>
2018-04-05 01:17:11 -0400

    Move sleep and song functionality to subthread

--- server.py
+++ server.py
@@ -2,9 +2,11 @@
 
 import sys
 import json
+import time
 import struct
 import socket
 import threading
+import collections
 import spotipy
 import spotipy.util
 
@@ -14,22 +16,23 @@ if len(sys.argv) != 2:
 
 port = int(sys.argv[1])
 
+
 # --------------------
 # perform Spotify setup
 # --------------------
 PLAYLIST_NAME = 'networks-playlist'
 
-def gen_songs(n):
+def gen_songs():
     """
-    Endlessly generate n-tuples of Spotify songs
+    Endlessly generate Spotify songs
     """
+    # buffer a multiple of 3 since we serve three choices per vote
+    BUFFER = 30
     SEED_GENRES = ['work-out', 'summer', 'club']
     while True:
-        recs = sp.recommendations(seed_genres=SEED_GENRES, limit=20)['tracks']
-        for i in xrange(0, len(recs), n):
-            group = tuple(recs[i:i+n])
-            if len(group) == n:
-                yield group
+        recs = sp.recommendations(seed_genres=SEED_GENRES, limit=30)['tracks']
+        for song in recs:
+            yield song
 
 # prompt for username and attempt authentication
 username = raw_input('What is your Spotify username?\n> ') 
@@ -77,9 +80,67 @@ UNREGISTER_MSG = "5"
 # --------------------
 # setup data stuctures
 # --------------------
-#songs = gen_songs(2)
+songs = gen_songs()
+clients = []
+tally = collections.Counter()
+client_lock = threading.Lock()
+tally_lock = threading.Lock()
+sock_write_lock = threading.Lock()
 
-# sp.user_playlist_add_tracks(user, pl['id'], tracks)
+def parse_song(s):
+    d = {
+        'artist': s['album']['artists'][0]['name'],
+        'name':   s['name'],
+        'length': s['duration_ms'] / 1000,
+        'uri':    s['uri'],
+            
+    }
+    return d
+
+def disc_jockey(sock, songs, sock_write_lock, clients, client_lock, tally, tally_lock, spot):
+    winner = None
+
+    while True:
+        s1 = parse_song(next(songs))
+        s2 = parse_song(next(songs))
+        s3 = parse_song(next(songs))
+
+        options_message = json.dumps([s1, s2, s3]) 
+        with tally_lock:
+            tally.clear()
+            tally.update([s1['uri'], s2['uri'], s3['uri']])
+        with client_lock, sock_write_lock:
+            for client in clients:
+                sock.sendto(SONGS_MSG + options_message, client)
+                sys.stderr.write("Just sent songs to client\n")
+
+        if not winner:
+            sys.stderr.write("Sleeping for 10 seconds\n")
+            time.sleep(10)
+        else:
+            # otherwise, sleep for time of winner
+            sys.stderr.write("Sleeping for %d seconds\n" % (winner['length'],))
+            time.sleep(winner['length'])
+
+        with tally_lock:
+            sys.stderr.write("Deciding the winner\n")
+            winner_uri, votes = tally.most_common(1)[0]
+            # decide which song this uri corresponds to
+            if s1['uri'] == winner_uri:
+                winner = s1
+            elif s2['uri'] == winner_uri:
+                winner = s2
+            else:
+                winner = s3
+            sys.stderr.write("Song %s has won with %d votes\n" % (winner, votes))
+
+        sys.stderr.write("trying to add track %s\n" % (winner['uri'],))
+        sys.stderr.write("playlist id: %s\n" % (pl['id'],))
+        spot.user_playlist_add_tracks(user, pl['id'], [winner['uri']])
+
+
+t = threading.Thread(target=disc_jockey, args=(sock, songs, sock_write_lock, clients, client_lock, tally, tally_lock, sp))
+t.start()
 
 while True:
     data, address = sock.recvfrom(MAX_DATA)
@@ -93,15 +154,27 @@ while True:
     msg = data[0]
     if msg == REGISTER_MSG:
         sys.stderr.write("Got a register message\n")
+        with client_lock, sock_write_lock:
+            clients.append(address)
+            sock.sendto(ACK_MSG, address)
+            sys.stderr.write("Sent ACK message\n")
     elif msg == VOTE_MSG:
         sys.stderr.write("Got a vote message\n")
         uri = data[1:]
+        with tally_lock:
+            if uri in tally:
+                tally[uri] += 1
+                sys.stderr.write("Recorded vote\n")
+            else:
+                sys.stderr.write("uri not found in tally\n")
     elif msg == UNREGISTER_MSG:
         sys.stderr.write("Got an unregister message\n")
-        pass
+        with client_lock:
+            try:
+                clients.remove(address)
+                sys.stderr.write("Removed client from list\n")
+            except ValueError:
+                sys.stderr.write("Could not remove client\n")
     else:
         sys.stderr.write("Unrecognized message type: %d\n", msg)
         continue
-    print address
-    sys.stderr.write("About to send ACK\n")
-    sock.sendto(ACK_MSG, address)
